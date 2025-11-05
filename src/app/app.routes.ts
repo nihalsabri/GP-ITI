8b5414e9bd0a8ebdd5a50bf95f456a438db7ddf4
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-11-06 00:52:30 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,21 @@ import { Routes } from '@angular/router';
 import { Layout } from './components/layout/layout';
 import { NoPage } from './components/no-page/no-page';
 import { Orders } from './components/orders/orders';
-import { Clients } from './components/clients/clients';
+import { ClientsComponent } from './components/clients/clients';
 import { Services } from './components/services/services';
 import { Tradespeople } from './components/tradespeople/tradespeople';
 
-export const routes: Routes = [ 
-    
-    {path:'',component:Layout, children:[
-
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'orders', component: Orders },
-      { path: 'clients', component: Clients },
+      { path: 'clients', component: ClientsComponent },
       { path: 'services', component: Services },
-      { path: 'tradespeople', component: Tradespeople }
-
-  ]},
-  {path:'**',component:NoPage,title:"Not Found"}
+      { path: 'tradespeople', component: Tradespeople },
+    ],
+  },
+  { path: '**', component: NoPage, title: 'Not Found' },
 ];
